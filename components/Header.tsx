02673ab0da7295c7758714a6e97c4ee382d645e1
hindustantech <kumarrupees507@gmail.com>
2025-08-14 16:11:30 +0530

    'add'

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,10 +8,7 @@ export default function Header() {
           {/* Logo */}
           <div className="flex items-center">
             <div className="flex items-center space-x-2">
-              <div className="w-8 h-8 bg-emerald-500 rounded-full flex items-center justify-center">
-                <span className="text-white font-bold text-sm">L</span>
-              </div>
-              <span className="text-xl font-bold text-gray-900">Logicspice</span>
+              <img src="/logo.png" alt="" />
             </div>
           </div>
 
